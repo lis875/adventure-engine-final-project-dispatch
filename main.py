1bb01bcb2b7f3ed1ed1167aba5f8d0ee526e8207
Dongjiyi Er <der2@wisc.edu>
2025-12-04 16:24:45 -0600

    main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+attribute = {
+    'HP' : 100,
+    'SAN' : 100,
+    'Inventory' : []
+}
+
+main_story = {
+    'Chapter_1' : {
+        'Chapter_1' : 'Chapter_1.txt',
+        'Chioces' : {
+            '1' : 'sec1',
+            '2' : 'sec2'
+        },
+    },
+}
+
+print(main_story)
